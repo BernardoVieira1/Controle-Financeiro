c52e0b4d5d665d025feccdef51092b5a53ce5225
Bernardo Vieira <bernardojvieira50@gmail.com>
2023-11-20 14:17:07 -0300

    Atualização backend

--- backend/prisma/migrations/20230907042003_create_model_transaction/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "transactions" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "value" REAL NOT NULL,
-    "dateCreate" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "type" TEXT NOT NULL,
-    "categoria" TEXT NOT NULL
-);
